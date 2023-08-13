96225ba61fb26b4427ba7dc2d7730934651798c7
mohamedabdulkadir <mohamed.abdulkadir7711@gmail.com>
2023-08-12 23:39:53 -0700

    Our first commit

---
+++ githubcollobration/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+      Hello my funs i'm here to teach you github collobrations
+    </div>
+    </>
+  )
+}
+
+export default App
